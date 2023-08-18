ff30f9c67b7e7e3ec6551b62a2f9df3da3f2bb6c
milreena <123353492+milreena@users.noreply.github.com>
2023-08-18 15:19:56 +0530

    Create user and password.c

---
+++ user and password.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+char message[] = "You are all very talented engineer!";
+char user[] = "sushmi";
+char password[] = "admin";
+void print()
+{
+    printf("Good aftrnoon\n");
+    printf("you are very active in the class!");
+
+}
